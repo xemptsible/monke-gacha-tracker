437befdb0978176426359ffc9296272704783e63
MT <burnerforthee@proton.me>
2025-04-20 07:38:54 +0700

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import ThemeProvider from "@/components/theme-provider";
-import ThemeToggle from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import Image from "next/image";
+import ThemeProvider from "@/components/providers/theme-provider";
+import Header from "@/components/header/header";
 
 export const metadata: Metadata = {
   title: "Monke",
@@ -19,36 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`flex flex-col antialiased`}>
+      <body className={`flex h-full flex-col antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <header className="sticky backdrop-blur-md top-0 z-auto flex w-full items-center justify-between p-2">
-            <Button id="home-button" variant={"link"} asChild>
-              <Link href={"/"}>
-                <Image
-                  id="feelsbadman-icon"
-                  src={"/feelsbadman.webp"}
-                  alt={""}
-                  width={32}
-                  height={32}
-                />
-                <Image
-                  id="ree-icon"
-                  src={"/ree.webp"}
-                  alt={""}
-                  width={32}
-                  height={32}
-                />
-                Home
-              </Link>
-            </Button>
-            <ThemeToggle />
-          </header>
-          {children}
+          <main className="h-full">
+            <Header />
+            <div className="container mx-auto px-4">{children}</div>
+          </main>
         </ThemeProvider>
       </body>
     </html>
