2add1dc80ebb8a1aa54891086b7c409e4050e333
MT <burnerforthee@proton.me>
2025-04-20 07:45:58 +0700

    Add a Deichgraf map

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,36 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <main className="mx-auto my-4 flex w-[90%] grow flex-col justify-center sm:my-8 sm:w-[70%]">
-        <div className="mb-12 flex flex-col items-center gap-4">
-          <Image
-            src={"/monke.webp"}
-            alt={
-              "An animated GIF depicting Pepega the Frog running while screaming"
-            }
-            width={100}
-            height={100}
-            unoptimized
-            priority
-            className="h-[100px] w-[100px]"
-          />
-          <h1 className="text-center text-2xl font-bold text-nowrap sm:text-3xl">
-            Monke Resources
-          </h1>
-        </div>
-        <div className="flex grow flex-col flex-wrap gap-4 sm:grow-0 sm:flex-row">
-          <Button
-            asChild
-            className="gacha-game flex-1 shrink-0 basis-0 p-0 sm:h-[20rem]"
-          >
+      <div className="flex flex-col items-center">
+        <Image
+          src={"/monke.webp"}
+          alt={
+            "An animated GIF depicting Pepega the Frog running while screaming"
+          }
+          width={100}
+          height={100}
+          unoptimized
+          priority
+          className="h-[100px] w-[100px]"
+        />
+        <h1 className="text-2xl font-bold text-nowrap sm:text-3xl">
+          Monke Resources
+        </h1>
+      </div>
+      <div className="flex grow flex-col justify-center gap-4">
+        <div className="flex">
+          <Button asChild className="gacha-game shrink-0 grow basis-80 p-0">
             <Link href={"/blue-archive"}>
               <span className="ba-cover size-full content-center text-center text-2xl font-light text-white">
                 Blue Archive
               </span>
             </Link>
           </Button>
-          <Button
-            className="gacha-game flex-1 shrink-0 basis-0 p-0 sm:h-[20rem]"
-            asChild
-          >
+          <Button asChild className="gacha-game shrink-0 grow basis-80 p-0">
             <Link href={"/gfl2"}>
               <span className="gfl2-cover size-full content-center text-center text-2xl font-light text-white">
                 Girl&apos;s Frontline 2
@@ -45,7 +38,7 @@ export default function Home() {
             </Link>
           </Button>
         </div>
-      </main>
+      </div>
     </>
   );
 }
